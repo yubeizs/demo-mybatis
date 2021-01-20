333c006e889348e1e40240ac2505f6a6741597da
June <zsah2012@163.com>
2021-01-20 23:24:27 +0800

    新建mybatis调试项目

---
+++ demo-mybatis/src/test/java/com/zs/sky/demomybatis/DemoMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zs.sky.demomybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
